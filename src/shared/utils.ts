d50d204198a32ae8f6a79107f9d17dca51a0def1
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-09-19 11:06:53 -0400

    Update utils.ts

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,4 +1,4 @@
-export const LIGHTNING_ADDRESS = 'vince@getalby.com';
+export const LIGHTNING_ADDRESS = 'lettergen@getalby.com';
 
 // export const MILLISATS_PER_MESSAGE = 200_000;
 export const MILLISATS_PER_MESSAGE = 30_000;
