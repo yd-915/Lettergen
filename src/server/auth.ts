435f1d1c6a2f548a25be1b5d5b5cdca4dc15bd60
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-09-20 22:58:49 -0400

    Update auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,8 +7,8 @@ export async function getUserFields(_context: unknown, args: any) {
 }
 
 export function config() {
-  const clientID = process.env.GOOGLE_CLIENT_ID 
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+  const clientID = "451488821164-59l9qq00c97sarq5ci79g53t93ga9u0o.apps.googleusercontent.com " 
+  const clientSecret = "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"
   return {
     clientID, // look up from env or elsewhere,
     clientSecret, // look up from env or elsewhere,
